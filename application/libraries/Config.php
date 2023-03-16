143f907a9366278b797891590bf8878380d75deb
ubuntu1 <miralgaywala@gmail.com>
2023-03-16 16:49:34 +0530

    database config

--- application/libraries/Config.php
+++ application/libraries/Config.php
@@ -3,16 +3,16 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Test@12345');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'achromic_lab');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', 'Test@12345');
+// define('DB_HOST', 'localhost');
+// define('DB_NAME', 'achromic_lab');
 
-// define('DB_USERNAME','leocamq9_dia');
-// define('DB_PASSWORD','di!@87ab23');
-// define('DB_HOST','localhost');
-// // define('DB_HOST','leocan.co');
-// // define('DB_NAME','leocamq9_leocanadmin');
-// define('DB_NAME','leocamq9_achromic_db');
+define('DB_USERNAME','leocamq9_dia');
+define('DB_PASSWORD','di!@87ab23');
+define('DB_HOST','localhost');
+// define('DB_HOST','leocan.co');
+// define('DB_NAME','leocamq9_leocanadmin');
+define('DB_NAME','leocamq9_achromic_db');
 
 ?>
