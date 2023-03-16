554ce026d3a7cd22d66a83976d57476be78c4658
ubuntu1 <miralgaywala@gmail.com>
2023-03-16 20:23:00 +0530

    remove eco

--- application/libraries/DbHandler.php
+++ application/libraries/DbHandler.php
@@ -209,7 +209,6 @@ class DbHandler
     public function addPacketDetails($company_id,$selectedDate,$packetNum,$quantity,$total_carat,$pending_process_qty_diamond,$pending_process_qty_carat,$broken_qty_diamond,$broken_qty_carat,$price_per_carat)
     {
         $sql_query = "insert into `packet` (`company_id`,`date`,`packet_no`,`packet_dimond_caret`,`packet_dimond_qty`,`pending_process_diamond_qty`,`pending_process_diamond_carat`,`broken_diamond_qty`,`broken_diamond_carat`,`price_per_carat`) VALUES ($company_id,'$selectedDate',$packetNum,$quantity,$total_carat,$pending_process_qty_diamond,$pending_process_qty_carat,$broken_qty_diamond,$broken_qty_carat,$price_per_carat)";
-        echo $sql_query;
         $stmt = $this->conn->prepare($sql_query);
         $stmt->execute();
         $stmt->close();
