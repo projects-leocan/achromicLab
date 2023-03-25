0f2cc0b183ab966e410b755d6a03459b5c891454
monikap0612 <monikap0612@gmail.com>
2023-03-25 10:44:01 +0530

    Update custom.js

--- application/libraries/custom.js
+++ application/libraries/custom.js
@@ -1113,7 +1113,7 @@ $(document).on("click", "#packet_id", function (event) {
 $('#packet_details_submit').click((e) => {
 
     let id = localStorage.getItem("packet_id");
-    let selectedDate = ($("#selected_date").val()).format('YYYY/MM/DD');
+    let selectedDate = ($("#selected_date").val()).format('YYYY-MM-DD');
     let company_id = localStorage.getItem("selecteCompanyID");
     let company_name = $("#selectedCompanyName").val()
     let packetNum = $("#number_of_packet").val();
