75ab3d265cc98db10d74933551844ba471ceefb2
monikap0612 <monikap0612@gmail.com>
2023-03-25 10:59:55 +0530

    Update custom.js

--- application/libraries/custom.js
+++ application/libraries/custom.js
@@ -1114,7 +1114,13 @@ $('#packet_details_submit').click((e) => {
 
     let id = localStorage.getItem("packet_id");
     let selectedDate = $("#selected_date").val();
-    //selectedDate = selectedDate.split("/").reverse().join("-");
+    var mydate = new Date(selectedDate);
+            year = mydate.getFullYear();
+            month = (mydate.getMonth() + 1).toString().padStart(2, "0");
+            day = mydate.getDate().toString().padStart(2, "0");
+            selectedDate = year+ '/' + month + '/' + day;
+
+    
     console.log("selected date", selectedDate);
     let company_id = localStorage.getItem("selecteCompanyID");
     let company_name = $("#selectedCompanyName").val()
