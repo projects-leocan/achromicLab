334e0e4f7ee80861a2a74c62ecdfe29b338026bd
macmini3 <macmini3@users.noreply.github.com>
2023-03-29 01:57:12 +0530

    new format

--- application/views/invoice.php
+++ application/views/invoice.php
@@ -1,4 +1,39 @@
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        $("#btnPrint").live("click", function () {
+            var divContents = $("#download_content").html();
+            var printWindow = window.open('', '', 'height=400,width=800');
+            printWindow.document.write('<html><head><title>DIV Contents</title>');
+            printWindow.document.write('</head><body >');
+            printWindow.document.write(divContents);
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.print();
+        });
+    </script>
+<title>Invoice</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+    	body{
+    
+    background:#eee;    
+}
+.side-logo h1 {
+        text-transform: uppercase;
+        color: #d4af37;
+    }
+
+    .side-logo p {
+        text-transform: uppercase;
+        width: 52%;
+        /* line-height: 0.3; */
+    }
     .container {
         display: flex;
         align-items: center;
@@ -10,59 +45,224 @@
 
     }
 
-    .side-logo h1 {
-        text-transform: uppercase;
-        color: #d4af37;
-    }
-
-    .side-logo p {
-        text-transform: uppercase;
-        width: 52%;
-        /* line-height: 0.3; */
-    }
-
-    .imgBox {
-        height: 150px;
-        width: 150px;
-
-    }
-    .customer{
-        width: 100%;
-    }
-
-</style>
-<div class="container">
-    <div class="left-div side-logo mx-5 mt-4">
-        <h1>achromic lab llp</h1>
-        <p> 32, Ground Floor,akshar dimoand market,
-            mini bazar varachha road,surat,gujarat - 395004 <br>
-            Call : 99744 27300 , 79841 02715
-            <br>
-            Email : achromiclabllp1974@gmail.com
-        </p>
-    </div>
-    <div class="right-div">
-        <img src="http://localhost/achromicLab/dist/img/company-logo.jpeg" class="imgBox" alt="">
-    </div>
-
     
+    </style>
+</head>
+
+
+<body>
+<div class="container bootdey" id="download_content">
+<div class="row invoice row-printable" style="padding-top: 25px;">
+<div class="col-md-10">
+
+<div class="panel panel-default plain" id="dash_0">
+
+<div class="panel-body p30">
+<div class="row">
+
+
+
+<div class="col-lg-6">
+
+<div class="invoice-from">
+<!-- <ul class="list-unstyled text-left"> -->
+<!-- <li> <h1>achromic lab llp</h1></li>
+<li>2500 Ridgepoint Dr, Suite 105-C</li>
+<li>Austin TX 78754</li>
+<li>VAT Number EU826113958</li> -->
+<div class="left-div side-logo">
+<ul class="list-unstyled text-left">
+        <h1>achromic lab llp</h1>
+        <li>32, Ground Floor,akshar dimoand market,</li>
+        <li>mini bazar varachha road,surat, gujarat - 395004</li>
+        <li>Call: 99744 27300, 79841 02715</li>
+        <li>Email: achromiclabllp1974@gmail.com</li>
+</ul>
+    </div>
+<!-- </ul> -->
 </div>
-<div class="container">
-    <section class="customer">
-        <div class="customer-data mx-5">
-            <table class="w-100">
+</div>
+
+
+<div class="col-lg-6">
+<ul class="list-unstyled text-right">
+<div class="invoice-logo"><img src="http://localhost/achromicLab/achromicLab/dist/img/logo1.png" alt="Invoice logo"></div>
+
+</ul>
+</div>
+
+
+<div class="col-lg-12">
+
+<div class="invoice-details mt25">
+<div class="well">
+<!-- <ul class="list-unstyled mb0">
+<li><strong>From:</strong> Anil Gems</li>
+<li><strong>Challan No:</strong> #936988</li>
+<li><strong>Invoice Date:</strong> 23/03/2023</li>
+</ul> -->
+           <table class="w-100">
                 <tr>
-                    <td>From : </td>
-                    <td>Mo :</td>
+                    <td width="300px">From : <span>ANIL GAMES</span></td>
+                    <td>Mo:</td>
+                    
                 </tr>
                 <tr>
-                    <td>Date : </td>
-                    <td>Challan No :</td>
-                    <td>Ref No :</td>
+                    <td >Date: <span>23/03/23</span></td>
+                    <td>Challan No: <span>121</span></td>
+                    <td>Ref No:</td>
                 </tr>
             </table>
-        </div>
-    
-    </section>
+</div>
+</div>
+
+<div class="invoice-items">
+<!-- <div class="table-responsive"> -->
+<table class="table table-bordered">
+<thead>
+<tr>
+
+                        
+<th class="per70 text-center">No.</th>
+<th class="per70 text-center">Lot No</th>
+<th class="per70 text-center">Remark/Description</th>
+<th class="per5 text-center">Pcs</th>
+<th class="per25 text-center">Weight</th>
+<th class="per25 text-center">Rate</th>
+<th class="per25 text-center">Amount</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+    <td class="text-center">1</td>
+    <td></td>
+    <td>1024MB Cloud 2.0 Server</td>
+    <td class="text-center">1</td>
+    <td class="text-center">$25.00 USD</td>
+    <td></td>
+    <td></td>
+
+</tr>
+<tr>
+    <td class="text-center"> 1</td>
+    <td></td>
+    <td>1024MB Cloud 2.0 Server</td>
+    <td class="text-center">1</td>
+    <td class="text-center">$25.00 USD</td>
+    <td></td>
+    <td></td>
+</tr>
+<tr>
+    <td class="text-center">1</td>
+    <td></td>
+    <td>1024MB Cloud 2.0 Server</td>
+    <td class="text-center">1</td>
+    <td class="text-center">$25.00 USD</td>
+    <td></td>
+    <td></td>
+</tr>
+</tbody>
+<tfoot>
+<tr>
+<th colspan="3" class="text-right">Sub Total:</th>
+<th class="text-center">12</th>
+<th class="text-center">$237.00 USD</th>
+</tr>
+<tr>
+<th colspan="3" class="text-right">Add/Less:</th>
+<th class="text-center"></th>
+<th class="text-center"></th>
+</tr>
+
+<tr>
+<th colspan="3" class="text-right">Net Amount:</th>
+<th class="text-center"></th>
+<th class="text-center"></th>
+</tr>
+
+<tr>
+<th colspan="7" class="text-left">Total Amount In Rupees:</th>
+</tr>
+
+<tr>
+<td colspan="2" class="text-center">Authorised Sign</td>
+<td colspan="2" class="text-center">Inssuer Sign</td>
+<td colspan="2" class="text-center">Delivery Sign</td>
+<td colspan="1" class="text-center">Customer Sign</td>
+</tr>
+
+<tr>
+<td colspan="2" class="text-center">&nbsp;</td>
+<td colspan="2" class="text-center">&nbsp;</td>
+<td colspan="2" class="text-center">&nbsp;</td>
+<td colspan="1" class="text-center">&nbsp;</td>
+</tr>
+
+<tr>
+<td colspan="7" class="text-left">HTHP પ્રોસેસ દરમિયાન ઉચ્ચ દબાણ ના પરિણામે ડાયમંડ માં નુકશાન થવાની પુરેપુરી સંભાવના છે. આ સ્થિતિથીની સંભવનના ૧૦૦ એ ૩% છે.HTHP દરમિયાન માલ ના તમામ નુકસાન થવાના જોખમને ગાહક દ્વારા પ્રતિબંધિત કરવામાં આવશે. ર) માલ સાથેની બીલ ની બે નકલોમાંથી,કૃપા કરી ને તમારા દ્વારા પ્રાપ્ત થયેલી  કૃતી ના પુરાવા રૂપે એક સહી કરેલ નકલ પરત કરો.એકવાર માલ ની રસીદ બિલ પર  હસ્ત્રક્ષાર કરીને પુષ્ટિ થઇ ગયા પછી કોઈ દાવા.દલીલો અથવા ફરિયાદો જેમકે વજન,શુદ્ધતા,ચારણી,કેટેગરી અને હિરા ની ગુણવતા ને ધ્યાન માં લેવામાં આવશે નહિ.3)બધા દાવાઓ સુરત અધિકારક્ષેત્રને આધિન છે. </th>
+</tr>
+
+<!-- <tr>
+<th colspan="2" class="text-right">20% VAT:</th>
+<th class="text-center">12</th>
+<th class="text-center">$47.40 USD</th>
+</tr>
+<tr>
+<th colspan="2" class="text-right">Credit:</th>
+<th class="text-center"></th>
+<th class="text-center">$00.00 USD</th>
+</tr>
+<tr>
+<th colspan="2" class="text-right">Total:</th>
+<th class="text-center">$284.4.40 USD</th>
+</tr> -->
+</tfoot>
+</table>
+<!-- </div> -->
+</div>
+<div class="invoice-footer mt25">
+<p class="text-center"> <a href="#" id="download_btn" class="btn btn-default ml15"><i class="fa fa-download mr5"></i> Download</a></p>
+</div>
+</div>
 
 </div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<!-- <script src="js/jsPDF/dist/jspdf.umd.js"></script>
+<script type="text/javascript">
+
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#download_btn').click(function () {
+    doc.fromHTML($('#download_content').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.save('sample-file.pdf');
+});
+
+
+</script> -->
+</body>
+</html>
+
+
+
+
+
+
