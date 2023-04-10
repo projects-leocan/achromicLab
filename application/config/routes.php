7cee9dc0533f2879e203ed2f882359081fe60001
ubuntu1 <miralgaywala@gmail.com>
2023-04-10 14:00:46 +0530

    updated routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,14 +7,14 @@ $route['default_controller'] = 'Dashboard';
 $route['packet'] = 'Dashboard/packet_menu';
 $route['company'] = 'Dashboard/company_menu';
 $route['packet_form'] = 'Dashboard/packet_form';
-// $route['invoice_form'] = 'Dashboard/invoice_form';
-$route['invoice_form'] = 'Dashboard/invoice_form_new';
+$route['invoice_form'] = 'Dashboard/invoice_form';
+// $route['invoice_form'] = 'Dashboard/invoice_form_new';
 
 
 $route['home'] = 'Dashboard';
 $route['signIn'] = 'Home';
-// $route['invoice'] = 'Home/print';
-$route['invoice'] = 'Home/print_new';
+$route['invoice'] = 'Home/print';
+// $route['invoice'] = 'Home/print_new';
 
 
 $route['404_override'] = '';
