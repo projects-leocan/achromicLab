861d1ecc338fe5766cefa514169e40d3402dbee3
ubuntu1 <miralgaywala@gmail.com>
2023-08-14 14:02:30 +0530

    remove extra cdns

--- application/views/footer.php
+++ application/views/footer.php
@@ -17,24 +17,9 @@
 <script src="plugins/jquery-validation/jquery.validate.min.js"></script>
 <script src="plugins/jquery-validation/additional-methods.min.js"></script>
 
-<!-- AdminLTE App -->
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-
-<!-- bs-custom-file-input -->
-<script src="plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
-
 <!-- popup -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-
-<!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
-
-<!-- jQuery Knob Chart -->
-<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
-
 
 <!--Updated: Added new version of jQueryUI CDN.-->
 <link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet"
@@ -45,50 +30,6 @@
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js"></script>
-<!-- for csv -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/jszip.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.8.0/xlsx.js"></script>
-
-<script>
-$(function() {
-    bsCustomFileInput.init();
-});
-</script>
-
-<script>
-// $(function () {
-//   //Initialize Select2 Elements
-//   $('.select2').select2()
-// });
-</script>
-
-<!--- pagination --->
-<!--<script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
-
-</script>
-
-<!-- <script>
-  $(function () {
-    $("#contact_us_tbody").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      // "columnDefs": [
-      //   { "orderable": false, "targets": [ 4 ] },
-      //   { "orderable": true, "targets": [ 2, 3 ] }
-      //]
-      //   "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).container().appendTo('#creer_wrapper .col-md-6:eq(0)');
-    $('#contact_us_tbody').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-</script> -->
-
 
 
 <!-- old data table cdn -->
@@ -109,23 +50,10 @@ $(function() {
 <!-- data range picker -->
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/plug-ins/preview/scrollToTop/dataTables.scrollToTop.min.js"></script>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
-<!-- Summernote -->
-<script src="plugins/summernote/summernote-bs4.min.js"></script>
 
-<!-- ckeditor -->
-<!-- <script src="../../../frutika_admin_demo_jahanvi/asset/ckeditor_4.20.0_standard/ckeditor/adapters/jquery.js"></script> -->
-<!-- <script src="../../../frutika_admin_demo_jahanvi/asset/ckeditor_4.20.0_standard/ckeditor/ckeditor.js"></script> -->
-<!-- <script src="https://cdn.ckeditor.com/4.20.0/standard/ckeditor.js"></script> -->
-<!-- <script src="/asset/ckeditor_4.20.0_standard/ckeditor/ckeditor.js"></script> -->
-<!-- <script src="/asset/ckeditor_4.20.0_standard/ckeditor/adapters/jquery.js"></script> -->
-
-
-
-<script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/classic/ckeditor.js"></script>
-<!-- <script src="bootstrap-autocomplete.min.js"></script> -->
-<!--<script src="https://cdn.jsdelivr.net/gh/xcash/bootstrap-autocomplete@master/dist/latest/bootstrap-autocomplete.min.js"></script>-->
 
 <!-- drop down with autocomplete -->
 <script src="https://cdn.jsdelivr.net/gh/xcash/bootstrap-autocomplete@v2.3.7/dist/latest/bootstrap-autocomplete.min.js">
